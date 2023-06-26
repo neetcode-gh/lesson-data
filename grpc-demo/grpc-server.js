24c78763a9c8a8fed01f2601605ffbe6c6b57e54
neetcode <neetcodebusiness@gmail.com>
2023-06-26 04:40:15 -0700

    comments grpc

--- grpc-demo/grpc-server.js
+++ grpc-demo/grpc-server.js
@@ -2,19 +2,20 @@ const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 const packageDefinition = protoLoader.loadSync('./protos/helloworld.proto', {});
 
-// Create a gRPC server and register the handlers for the defined RPCs
 const server = new grpc.Server();
 const hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+
+// Define and register helloworld RPC handler
+function sayHello(call, callback) {
+    callback(null, {message: 'Hello ' + call.request.name});
+}
 
 server.addService(hello_proto.HelloWorld.service, {
     SayHello: sayHello
 });
+
+// Create a gRPC server and listen on port 50051
 server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
     server.start();
     console.log('Server running on port 50051');
 });
-
-// Synchronous function to handle the SayHello RPC
-function sayHello(call, callback) {
-    callback(null, {message: 'Hello ' + call.request.name});
-}
--- grpc-demo/rest-server.js
+++ grpc-demo/rest-server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
-app.use(express.json()); // for parsing application/json
+app.use(express.json());
 
+// Define and register /sayHello REST endpoint
 app.post('/sayHello', (req, res) => {
     const name = req.body.name;
     res.json({ message: 'Hello ' + name });
 });
 
+
+// Create an HTTP server and listen on port 3000
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
 });
